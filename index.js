02f94cce42bdeed8b47880cb95757a0e42ed4139
CND1468DX1 <jose.gallego@celuweb.com>
2022-11-30 14:49:29 -0500

    Fin seccion 3

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+//App express
+const app = express();
+
+
+const path = require('path');
+require ('dotenv').config();
+
+
+
+
+//Node server
+const server = require('http').createServer(app);
+module.exports.io = require('socket.io')(server);
+require('./sockets/socket');
+
+
+
+//Path publico
+const publicPath = path.resolve(__dirname, 'public');
+app.use (express.static(publicPath));
+
+
+
+server.listen(process.env.PORT, (err) => {
+    if (err) throw new Error(err);
+    console.log('Servidor corriendo en puerto!!!', process.env.PORT);
+});
